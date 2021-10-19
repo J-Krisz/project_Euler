a7f983ebec47f8952895feda079af3e1baff3ca6
J-Krisz <jkristi4n@gmail.com>
2021-10-19 01:08:18 +0100

    problem 9

--- Problem 9 - Special Pythagorean triplet.py
+++ Problem 9 - Special Pythagorean triplet.py
@@ -2,4 +2,14 @@
 # For example, 32 + 42 = 9 + 16 = 25 = 52. 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.Find the product abc. 
 
+def is_pythagorean(a, b, c):
+    return a**2 + b**2 == c**2
 
+SUM =1000
+
+for a in range(1,500):
+    for b in range(a+1, 500):
+        c = SUM - a - b
+        if is_pythagorean(a, b, c):
+            if a + b + c == SUM:
+                print(a * b * c)
